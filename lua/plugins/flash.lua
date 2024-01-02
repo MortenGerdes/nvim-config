a9eb8a2adbdea03abd0e6439a37a8a1a828c7508
Morten Gerdes <mgd@lunar.app>
2024-01-02 22:25:23 +0100

    Add dap config and transparent background config

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "folke/flash.nvim",
+        opts = {
+            label = {
+                uppercase = false,
+            },
+            rainbow = {
+                enabled = true,
+                -- number between 1 and 9
+                shade = 5,
+            },
+        },
+    },
+}
