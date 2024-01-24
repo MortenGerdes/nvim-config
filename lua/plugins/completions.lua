7b38acf5081a09bb8fb031b92985b48d8a658487
Morten Gerdes <mgd@lunar.app>
2024-01-24 11:28:28 +0100

    Add Dadbob

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,64 +1,69 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-nvim-lsp",
-            "onsails/lspkind.nvim",
-        },
-        config = function()
-            local cmp = require("cmp")
-            local lspkind = require("lspkind")
-            require("luasnip.loaders.from_vscode").lazy_load()
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            cmp.setup({
-                snippet = {
-                    -- REQUIRED - you must specify a snippet engine
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                    end,
-                },
-                formatting = {
-                    format = lspkind.cmp_format({
-                        with_text = true,
-                        menu = {
-                            buffer = "[buf]",
-                            nvim_lsp = "[LSP]",
-                            nvim_lua = "[api]",
-                            path = "[path]",
-                            luasnip = "[snip]",
-                        },
-                    }),
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                }),
-                sources = cmp.config.sources({
-                    { name = "luasnip" }, -- For luasnip users.
-                    { name = "nvim_lsp" },
-                    { name = "path" },
-                }, {
-                    { name = "buffer", keyword_length = 5 },
-                }),
-            })
-        end,
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        event = "InsertEnter",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-    },
+			local menu = {
+				buffer = "[buf]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[api]",
+				path = "[path]",
+				luasnip = "[snip]",
+				vim_dadbod_completion = "[db]", -- I can't get this to work
+			}
+            menu["vim-dadbod-completion"] = "[db]"
+
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						with_text = true,
+						menu = menu,
+					}),
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "vim-dadbod-completion" },
+				}, {
+					{ name = "buffer", keyword_length = 5 },
+				}),
+			})
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		event = "InsertEnter",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
 }
