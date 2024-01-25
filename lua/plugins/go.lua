670d6ffb32473adcd39e58928122f0174df47a9e
Morten Gerdes <mgd@lunar.app>
2024-01-25 22:42:40 +0100

    Use trouble to show errors in test

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,41 +1,55 @@
 return {
-    {
-        "ray-x/go.nvim",
-        ft = { "go", "gomod" },
-        dependencies = { -- optional packages
-            "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-        keys = {
-            { "<leader>cs", "<cmd>GoFillStruct<cr>", desc = "Go fill struct" },
-            { "<leader>ce", "<cmd>GoIfErr<cr>",      desc = "Go if err" },
-        },
-        config = function()
-            require("go").setup()
+	{
+		"ray-x/go.nvim",
+		ft = { "go", "gomod" },
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+		keys = {
+			{ "<leader>cs", "<cmd>GoFillStruct<cr>", desc = "Go fill struct" },
+			{ "<leader>ce", "<cmd>GoIfErr<cr>", desc = "Go if err" },
+		},
+		config = function()
+			require("go").setup()
 
-            local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*.go",
-                callback = function()
-                    require("go.format").goimport()
-                end,
-                group = format_sync_grp,
-            })
-        end,
-    },
-    {
-        "leoluz/nvim-dap-go",
-        ft = { "go", "gomod" },
-        config = function()
-            require("dap-go").setup({
-                delve = {
-                    build_flags = "-tags=integration",
-                },
-            })
+			local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*.go",
+				callback = function()
+					require("go.format").goimport()
+				end,
+				group = format_sync_grp,
+			})
+		end,
+	},
+	{
+		"leoluz/nvim-dap-go",
+		ft = { "go", "gomod" },
+		config = function()
+			require("dap-go").setup({
+				delve = {
+					build_flags = "-tags=integration",
+				},
+			})
+            -- stylua: ignore
             vim.keymap.set("n", "<leader>td", "<cmd>lua require('dap-go').debug_test()<cr>", {desc = "Debug test", silent = true })
-        end,
-    },
+		end,
+	},
+	{
+		"nvim-neotest/neotest",
+		optional = true,
+		dependencies = {
+			"nvim-neotest/neotest-go",
+		},
+		opts = {
+			adapters = {
+				["neotest-go"] = {
+					args = { "-tags=integration", "-count=1" },
+				},
+			},
+		},
+	},
 }
-
